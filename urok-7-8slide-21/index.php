69a693771d337d628a5520119c4f26d17e7447b1
Tsurkan Georgiy <egorizz@mail.ru>
2022-10-23 10:10:17 +0500

    03.10.2022

--- urok-7-8slide-21/index.php
+++ urok-7-8slide-21/index.php
@@ -11,22 +11,43 @@ $text = [
     "сегодня" => "Дата: ",
     "разность" => "Разность: "
 ];
-$now = date('d.m.Y');
-$date1 = date_create('1991-07-10');
-$date2 = date_create(date('d.m.Y'));
-$diff = date_diff($date1, $date2);
-$vowels = mb_str_split("АОУЫЭЕЁИЮЯаоуыэеёиюя");
-$words_num = 0;
-$vowels_num = 0;
-foreach($text as $string){
-    $words_num += count(explode(" ", $string));
-
+function diff_date($birthday)
+{
+    $now = date('d.m.Y');
+    $date1 = date_create(date($birthday));
+    $date2 = date_create(date('d.m.Y'));
+    return date_diff($date1, $date2);
+}
+function count_word($string)
+{
+    return count(explode(" ", $string));
+}
+function count_vowels($string){
+    $vowels_num = 0;
+    $vowels = mb_str_split("АОУЫЭЕЁИЮЯаоуыэеёиюя");
     $string = mb_str_split($string);
-    for($i = 0; $i < count($string); $i++){
-        if (in_array($string[$i], $vowels)){
+    for ($i = 0; $i < count($string); $i++) {
+        if (in_array($string[$i], $vowels)) {
             $vowels_num++;
         }
     }
+    return $vowels_num;
+}
+function paint_words($string){
+    $b = explode(" ", $string);
+    for ($i = 0; $i < count($b); $i++) {
+        if ($i % 2 == 0) {
+            echo '<span style="color: blue;">' . $b[$i] . " </span>";
+        } else {
+            echo '<span style="color: red;">' . $b[$i] . " </span>";
+        }
+    }
+}
+$words_num = 0;
+$vowels_num = 0;
+foreach ($text as $string) {
+    $words_num += count_word($string);
+    $vowels_num += count_vowels($string);
 }
 ?>
 <!DOCTYPE html>
@@ -41,34 +62,25 @@ foreach($text as $string){
 <body>
     <header class="header">
         <img src="Images/Smile.png">
-        <a href="table.html"><?php echo $text["таблица"]?></a>
-        <div><?php echo $text["гласные"].$vowels_num?></div>
-        <div><?php echo $text["слова"].$words_num?></div>
-        <div><?php echo $text["др"]?></div>
-        <div><?php echo $text["сегодня"].$now?></div>
-        <div><?php echo $text["разность"].$diff -> format('%a дней')?></div>
+        <a href="table.html"><?php echo $text["таблица"] ?></a>
+        <div><?php echo $text["гласные"] . $vowels_num ?></div>
+        <div><?php echo $text["слова"] . $words_num ?></div>
+        <div><?php echo $text["др"] ?></div>
+        <div><?php echo $text["сегодня"] . date('d.m.Y') ?></div>
+        <div><?php echo $text["разность"] . diff_date('10.07.1991')->format('%a дней') ?></div>
     </header>
     <main class="block">
         <aside class="photo"><img src="Images/Egorka.jpg"></aside>
         <div class="content">
             <div class="name">
-                <h2><?php echo $text["имя"]?></h2>
+                <h2><?php echo $text["имя"] ?></h2>
             </div>
             <div class="info">
-                <div class="information"><span style="color: blue;"><?php echo $text["приветствие"]?></span><?php echo $text["инфо"]?>
+                <div class="information"><span style="color: blue;"><?php echo $text["приветствие"] ?></span><?php echo $text["инфо"] ?>
                 </div>
                 <div class="comment">
-                    <?php 
-                    $b = explode(" ", $text["отзыв"]);
-                    for ($i = 0; $i < count($b); $i++){
-                        if ($i % 2 == 0){
-                            echo '<span style="color: blue;">'.$b[$i]." </span>";
-                        }
-                        else{
-                            echo '<span style="color: red;">'.$b[$i]." </span>";
-                        }
-                    }
-                    
+                    <?php
+                    paint_words($text["отзыв"])
                     ?></div>
             </div>
         </div>
